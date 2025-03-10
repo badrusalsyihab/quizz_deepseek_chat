abb595fb53b626ce9a78ebee8a1e3e7618f77a81
badrusalsyihab <badrusalsyihab@gmail.com>
2025-03-10 17:20:55 +0700

    fix connetion lib

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,5 +1,4 @@
 // app/api/categories/route.js
-import { createPool } from 'mysql2/promise';
 import { getDBConnection } from '@/lib/db'
 
 
@@ -7,18 +6,6 @@ export async function GET(request) {
     let connection;
     const { searchParams } = new URL(request.url);
     const kelas_id = searchParams.get('kelas_id');
-
-    // // Buat koneksi ke MySQL
-    // const pool = createPool({
-    //     host: '30vog.h.filess.io',
-    //     user: 'quizdeepseek_entirepet', // Ganti dengan username MySQL Anda
-    //     password: '5c7c25afb5d36e860bde166fe4d49d7893879f31', // Ganti dengan password MySQL Anda
-    //     database: 'quizdeepseek_entirepet', // Ganti dengan nama database Anda
-    //     waitForConnections: true,
-    //     connectionLimit: 10,
-    //     queueLimit: 0,
-    // });
-
     try {
 
         connection = await getDBConnection();
